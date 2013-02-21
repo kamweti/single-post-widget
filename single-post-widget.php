60240189513252fff32a3fa3f3e744fdac42c037
kamweti muriuki <kamwetimuriuki@gmail.com>
2013-02-21 15:33:07 +0300

    Replaced url text field with post select box

--- single-post-widget.php
+++ single-post-widget.php
@@ -17,128 +17,112 @@ class SinglePostWidget extends WP_Widget {
     private $domain = "single-post-widget";
 
     function __construct() {
-		$widget_ops = array(
-            'description' => __('Display single selected Post or Page.', $this->domain)
-        );
-		$control_ops = array('width' => 400, 'height' => 350);
-        parent::__construct(
-            false,
-            __('Single Post', $this->domain),
-            $widget_ops,
-            $control_ops
-        );
+      $widget_ops = array(
+        'description' => __('Display single selected Post or Page.', $this->domain)
+      );
+      $control_ops = array('width' => 400, 'height' => 350);
+      parent::__construct(
+        false,
+        __('Single Post', $this->domain),
+        $widget_ops,
+        $control_ops
+      );
     }
 
     public function form($instance) {
-        // outputs the options form on admin
-        $postid = (isset($instance['postid'])) ? $instance['postid'] : '';
-        $pid = $this->get_field_id('postid');
-        $pf = $this->get_field_name('postid');
-        echo '<p>';
-        echo __("Post or Page URL", $this->domain);
-        echo "<br />";
-        echo "<input type=\"text\" class=\"widefat\" id=\"{$pid}\" name=\"{$pf}\" value=\"{$postid}\" />";
-        echo '</p>';
 
-        $sizes = get_intermediate_image_sizes();
-        $size = (isset($instance['size']) && $instance['size']) ? $instance['size'] : '';
-        $sfield = $this->get_field_id('size');
-        $sfname = $this->get_field_name('size');
-        echo '<p>';
-        echo __('Image size:', $this->domain);
-        echo '<br />';
-        $op = '<option value="%s"%s>%s</option>';
-        echo "<select class=\"widefat\" id=\"{$sfield}\" name=\"{$sfname}\">";
-        printf($op, '', '', '');
-        foreach ($sizes as $s) {
-            if ($s === $size) {
-                printf($op, $s, ' selected="selected"', $s);
-            } else {
-                printf($op, $s, '', $s);
-            }
-        }
-        echo "</select>";
-        echo '</p>';
+      // outputs the options form on admin
+      $postid = (isset($instance['postid'])) ? $instance['postid'] : '';
+      $pid = $this->get_field_id('postid');
+      $pf = $this->get_field_name('postid');
+      $all = get_posts('numberposts=-1');
 
-        $tpl_value = (isset($instance['tpl']) && $instance['tpl']) ? $instance['tpl'] : $this->template();
-        $tpl_field = $this->get_field_id('tpl');
-        $tpl_fname = $this->get_field_name('tpl');
-        echo '<label for="'.$tpl_field.'">';
-        echo __("Template:", $this->domain);
-        echo '</label><br />';
-        printf(
-            '<textarea class="widefat" rows="16" cols="20" id="%s" name="%s">%s</textarea>',
-            $tpl_field,
-            $tpl_fname,
-            htmlentities($tpl_value, ENT_QUOTES, 'UTF-8')
-        );
 
-        $tags = array(
-            "%post_title%",
-            "%post_date%",
-            "%post_url%",
-            "%post_thumb%",
-            "%post_excerpt%",
-            "%class%",
-        );
 
-        echo '<div style="margin:5px 0;">';
-        echo '<code>'.join("</code>, <code>", $tags).'</code>';
-        echo '</div>';
+      if ( isset( $instance[ 'title' ] ) ) {
+        $title = $instance[ 'title' ];
+      } else {
+        $title = __( 'Featured Post', $this->domain );
+      }
+      ?>
+      <p>
+        <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+        <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+      </p>
+      <?php
+      echo '<p>';
+      echo __("Select the post or page", $this->domain);
+      echo "<br />";
+      echo '<select class="widefat" id="'.$pid.'" name="'.$pf.'">';
+      foreach ( $all as $post ) {
+        $selected = ( $post->ID == $postid )? 'selected' : '';
+        echo '<option value="'.$post->ID.'" '.$selected.'>'.$post->post_title.'</option>';
+      }
+      echo '</select>';
+      echo '</p>';
+
+      $sizes = get_intermediate_image_sizes();
+      $size = (isset($instance['size']) && $instance['size']) ? $instance['size'] : '';
+      $sfield = $this->get_field_id('size');
+      $sfname = $this->get_field_name('size');
+      echo '<p>';
+      echo __('Image size:', $this->domain);
+      echo '<br />';
+      $op = '<option value="%s"%s>%s</option>';
+      echo "<select class=\"widefat\" id=\"{$sfield}\" name=\"{$sfname}\">";
+      printf($op, '', '', '');
+      foreach ($sizes as $s) {
+          if ($s === $size) {
+              printf($op, $s, ' selected="selected"', $s);
+          } else {
+              printf($op, $s, '', $s);
+          }
+      }
+      echo "</select>";
+      echo '</p>';
+
     }
 
     public function update($new_instance, $old_instance) {
-        // processes widget options to be saved
-        return $new_instance;
+      // processes widget options to be saved
+      return $new_instance;
     }
 
     public function widget($args, $instance) {
-        $pid = null;
-        if (isset($instance['postid']) && preg_match("/^[0-9]+$/", $instance['postid'])) {
-            $pid = $instance['postid'];
-        } elseif (isset($instance['postid']) && $instance['postid']) {
-            $pid = url_to_postid($instance['postid']);
-        }
-        if (!$pid) {
-            return '';
-        }
-        $tpl  = ($instance['tpl']) ? $instance['tpl'] : $this->template();
-        if (!isset($instance['size'])) {
-            $instance['size'] = '';
-        }
+      $pid = null;
+      if (isset($instance['postid']) && preg_match("/^[0-9]+$/", $instance['postid'])) {
+          $pid = $instance['postid'];
+      } elseif (isset($instance['postid']) && $instance['postid']) {
+          $pid = url_to_postid($instance['postid']);
+      }
+      if (!$pid) {
+          return '';
+      }
 
-        $p = apply_filters('single_post_widget_post', get_post($pid));
 
-        echo $args['before_widget'];
-        echo $args['before_title'];
-        echo esc_html($p->post_title);
-        echo $args['after_title'];
+      if ( isset( $instance['title'] ) ) {
+        $title = apply_filters( 'widget_title', $instance['title'] );
+      } else {
+        $title = 'Featured Post';
+      }
 
-        $class = array(
-            $p->post_type.'-'.$pid,
-            $p->post_type,
-            'single-post-widget'
-        );
-        if ($instance['size']) {
-            $class[] = 'size-'.$instance['size'];
-        }
-        if ($instance['size']) {
-            $post_thumb = get_the_post_thumbnail($pid, $instance['size']);
-        } else {
-            $post_thumb = '';
-        }
-        $tpl = str_replace(
-            '%post_date%',
-            mysql2date(get_option("date_format"), $p->post_date, false),
-            $tpl
-        );
-        $tpl = str_replace('%post_title%', esc_html($p->post_title), $tpl);
-        $tpl = str_replace('%post_excerpt%', esc_html($p->post_excerpt), $tpl);
-        $tpl = str_replace('%post_thumb%', $post_thumb, $tpl);
-        $tpl = str_replace('%post_url%', esc_url(get_permalink($pid)), $tpl);
-        $tpl = str_replace('%class%', join(' ', $class), $tpl);
-        echo $tpl;
-        echo $args['after_widget'];
+      echo $args['before_widget'];
+      echo $args['before_title'];
+      echo esc_html($title);
+      echo $args['after_title'];
+
+      $the_post = new WP_Query(
+        array(
+          'p' => $pid,
+          'post_type'=> 'any'
+        )
+      );
+      while($the_post->have_posts()) :
+        $the_post->the_post();
+        get_template_part( 'content', 'featured' );
+      endwhile;
+
+      echo $args['after_widget'];
     }
 
     private function template()
@@ -150,41 +134,8 @@ class SinglePostWidget extends WP_Widget {
         return apply_filters("single-post-widget-template", $html);
     }
 }
+// register Foo_Widget widget
+add_action( 'widgets_init', create_function( '', 'register_widget( "SinglePostWidget" );' ) );
 
-class SinglePostWidgetInit {
-
-function __construct()
-{
-    add_action('widgets_init', array(&$this, "widgets_init"));
-    add_action("plugins_loaded", array(&$this, "plugins_loaded"));
-    add_action("wp_head", array(&$this, "wp_head"));
-}
-
-public function wp_head()
-{
-    $url = plugins_url("", __FILE__).'/style.css';
-    printf(
-        '<link rel="stylesheet" type="text/css" media="all" href="%s" />'."\n",
-        apply_filters("single-post-widget-stylesheet", $url)
-    );
-}
-
-public function widgets_init()
-{
-    return register_widget("SinglePostWidget");
-}
-
-public function plugins_loaded()
-{
-    load_plugin_textdomain(
-        "single-post-widget",
-        false,
-        dirname(plugin_basename(__FILE__)).'/languages'
-    );
-}
-
-}
-
-new SinglePostWidgetInit();
 
 ?>
